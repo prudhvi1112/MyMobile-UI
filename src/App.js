ce8e1c74dab91f03f9b47ec9a1ce336f115f0fdd
bhaskar-rau <bhaskar.rau.k@gmail.com>
2024-11-20 17:04:03 +0530

    created basic theme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./theme";
+import { Box, Typography } from "@mui/material";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Box className="App">
+        <Typography sx={{ color: "primary.main", textAlign: "center" }}>
+          Welcome to My Mobile
+        </Typography>
+      </Box>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,29 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1976d2",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+    background: {
+      default: "#f4f4f4",
+    },
+  },
+  typography: {
+    fontFamily: "Roboto, sans-serif",
+    h1: {
+      fontSize: "2rem",
+      fontWeight: 500,
+    },
+    h2: {
+      fontSize: "1.5rem",
+      fontWeight: 400,
+    },
+  },
+  spacing: 8,
+});
+
+export default theme;
